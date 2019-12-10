4e6815bb6f97bf13433ac9bd263196dccd7faf18
DiegoRmsR <Diegoramos.mt@gmail.com>
2019-12-10 02:34:05 +0000

    [Feature] manifest that kills a process named killmenow.

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# create a manifest that kills a process named killmenow.
+# manifest that kills a process named killmenow.
 
-exec {'killmenow':
-  command => '/usr/bin/pkill --full killmenow'
+exec { 'killmenow':
+  command => '/usr/bin/pkill -f killmenow'
 }
