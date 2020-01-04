4813bedc8e7072038ca9466c2faae4dfe1b22c57
DiegoRmsR <Diegoramos.mt@gmail.com>
2020-01-04 01:01:20 +0000

    [Feature] automate the task of creating a custom HTTP header

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,15 @@
+# Update all the O.S
+# Do the response
+
+exec {'Update':
+  provider => shell,
+  command  => 'sudo apt-get -y update; sudo apt-get -y install nginx; echo "Holberton School" | sudo tee /var/www/html/index.html',
+  before   => Exec['res'],
+}
+
+
+exec {'res':
+  environment => ["hn=${hostname}"],
+  provider => shell,
+  command  => 'sudo sed -i "11i\\\tadd_header X-Served-By $ht;" /etc/nginx/nginx.conf; sudo service nginx restart;',
+}
