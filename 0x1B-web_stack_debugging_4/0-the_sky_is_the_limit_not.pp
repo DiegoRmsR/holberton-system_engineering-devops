34dc19a571e246575a039f25639ad331a9e54568
DiegoRmsR <diegoramos.mt@gmail.com>
2020-02-26 20:31:53 -0500

    [Feature] bug fixed

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,11 @@
+#Featuring Nginx, change ULIMIT = "- n 15" for ULIMIT = "- n 4096"6" n 4096"6"
+
+exec {'change':
+  provider  => shell,
+  command   =>  'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/i" /etc/default/nginx',
+  before => Exec['restart'],
+}
+exec {'restart':
+  provider  => shell,
+  command   => 'sudo service nginx restart',
+}
